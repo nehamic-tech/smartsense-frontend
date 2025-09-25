fdc402e4be7be79bfcf5f5885a4bb56d2ae2d9a5
byteslash <serg.jn@gmail.com>
2025-09-25 17:40:21 +0200

    initial setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,34 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import antfu from "@antfu/eslint-config";
+import css from "@eslint/css";
 
-export default withNuxt(
-  // Your custom configs here
-)
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(antfu({
+  type: "app",
+  typescript: true,
+  vue: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+  rules: {
+    "vue/html-closing-bracket-newline": ["error", {
+      singleline: "never",
+      multiline: "never",
+    }],
+    "ts/consistent-type-definitions": ["error", "type"],
+    "no-console": "warn",
+    "node/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "node/no-process-env": ["error"],
+    "perfectionist/sort-imports": ["error", { tsconfigRootDir: "." }],
+    "css/no-empty-blocks": "error",
+  },
+  plugins: {
+    css,
+  },
+  language: "css/css",
+}),
+);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+import tailwindcss from "@tailwindcss/vite";
 
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxtjs/i18n",
+    "nuxt-translation-manager",
+  ],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  i18n: {
+    defaultLocale: "pt",
+    locales: [
+      { code: "pt", name: "Português", file: "pt.json" },
+      { code: "en", name: "English", file: "en.json" },
+    ],
+    langDir: "../app/locales/",
+    strategy: "prefix_except_default",
+  },
+});
